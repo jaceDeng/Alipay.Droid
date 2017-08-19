758df3770fedea574bf2a52b8f57cc374865788b
jsonsugar <djs@olexe.cn>
2017-08-19 22:21:17 +0800

    支付宝 android   xamrin绑定完成

---
+++ AliPay/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace AliPay
+{
+    [Activity(Label = "AliPay", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
